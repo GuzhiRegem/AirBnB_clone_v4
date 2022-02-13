3389f3f311d8bb5113cec28fcf057e9f44e196a3
#RodrigoMato00 <3293@holbertonschol.com>
2022-02-13 18:49:58 -0300

    3 js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,33 +1,48 @@
-$(document).ready(function () {
-  let lis = [];
-  let lis_n = [];
-  $('li input').on('click', function (data) {
-    const id = $(this).data('id');
-    if ($(this).is(':checked')) {
-      lis.push(id);
-      lis_n.push($(this).data('name'));
-    } else {
-      const idx = lis.indexOf(id);
-      if (idx !== -1) {
-        lis.splice(idx, 1);
-        lis_n.splice(idx, 1);
-      }
-    }
-    $('.amenities h4').text(String(lis_n));
-  });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    if (data.status === 'OK') {
-      $('')
-    }
-  });
-  $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/status/',
-    type: 'GET',
-    success: function(data){ 
-      $('div#api_status').addClass('available');
-    },
-    error: function(data) {
-      $('div#api_status').removeClass('available');
-    }
-  });
+window.addEventListener('DOMContentLoaded', (e) => {
+    let list = [];
+    $('input:checkbox').change(function() {
+        if ($(this).is(":checked")) {
+            const name = ($(this).attr('data-name'));
+            list.push(name);
+        } else {
+            const name = ($(this).attr('data-name'));
+            const i = list.indexOf(name);
+            list.splice(i, 1);
+        }
+        $('.amenities > h4').text(list.join(', '));
+    });
+    $.get('http://d701e46f5948.d79346c3.hbtn-cod.io:5001/api/v1/status/', function(data, status) {
+        if (status) {
+            $('div#api_status').addClass('available');
+        } else {
+            $('div#api_status').removeClass('available');
+        }
+    });
+    $.ajax({
+        type: "POST",
+        url: 'http://d701e46f5948.d79346c3.hbtn-cod.io:5001/api/v1/places_search/',
+        dataType: 'json',
+        async: false,
+        data: JSON.stringify({}),
+        contentType: 'application/json',
+        success: function(data) {
+            data.forEach(element => {
+                const article = `<article>
+          <div class="title_box">
+          <h2>${element.name}</h2>
+          <div class="price_by_night">${element.price_by_night}</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${element.max_guest}</div>
+                <div class="number_rooms">${element.number_rooms}</div>
+                <div class="number_bathrooms">${element.number_bathrooms}</div>
+        </div>
+        <div class="description">
+        ${element.description}
+        </div>
+      </article>`;
+                $('.places').append(article);
+            });
+        }
+    });
 });
